bcd2b1bb4fe5a3716c6e5b92e92912fd21dff6ae
John MacLeod <johnmacleod@johns-mbp-765.home>
2019-11-25 23:32:52 +1100

    hello

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='jhmfunctions',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=['dill'
+    ],
+)
