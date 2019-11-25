ca09f0db65cb01a8e568706690600f4fae177727
johnhmacleod <john.h.macleod@gmail.com>
2019-11-25 23:43:13 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jhmfunctions',
-    version='1.0.1',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=['dill'
     ],
