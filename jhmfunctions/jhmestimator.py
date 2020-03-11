5401904f298ce3236a9e9d0b5e3b374df7f271c1
johnmacleod <john.h.macleod@gmail.com>
2020-03-11 12:50:07 +1100

    Yes

--- jhmfunctions/jhmestimator.py
+++ jhmfunctions/jhmestimator.py
@@ -85,7 +85,77 @@ class JHMSimpleAnomalyX(BaseRegressor):
                        )
                       
         return (inputs,outputs)
-    
+
+
+class JHMSimpleAnomalyZ(BaseRegressor):
+    '''
+    Sample function uses a regression model to predict the value of one or more output
+    variables. It compares the actual value to the prediction and generates an alert
+    when the difference between the actual and predicted value is outside of a threshold.
+    '''
+    # class variables
+    train_if_no_model = True
+    estimators_per_execution = 3
+    num_rounds_per_estimator = 3
+
+    def __init__(self, features, targets, threshold,
+                 predictions=None, alerts=None):
+        super().__init__(features=features, targets=targets, predictions=predictions)
+        if alerts is None:
+            alerts = ['%s_alert' % x for x in self.targets]
+        self.alerts = alerts
+        self.threshold = threshold
+        # registration
+        self.inputs = ['features', 'target']
+        self.outputs = ['predictions', 'alerts']
+        self.constants = ['threshold']
+
+    def execute(self, df):
+
+        df = super().execute(df)
+        for i, t in enumerate(self.targets):
+            prediction = self.predictions[i]
+            df['_diff_'] = (df[t] - df[prediction]).abs()
+            alert = IoTAlertHighValue(input_item='_diff_',
+                                      upper_threshold=self.threshold,
+                                      alert_name=self.alerts[i])
+        df = alert.execute(df)
+
+        return df
+
+    @classmethod
+    def build_ui(cls):
+        # define arguments that behave as function inputs
+        inputs = []
+        inputs.append(ui.UIMultiItem(
+            name='features',
+            datatype=float,
+            description="Data items to use as features")
+        )
+        inputs.append(ui.UIMultiItem(
+            name='targets',
+            datatype=float,
+            description="Data items to use as targets")
+        )
+        inputs.append(ui.UISingle(
+            name='threshold',
+            datatype=float)
+        )
+        outputs = []
+        outputs.append(ui.UIFunctionOutMulti(
+            name='predictions',
+            datatype=float,
+            cardinality_from='targets',
+            description='Output predictions')
+        )
+        outputs.append(ui.UIFunctionOutMulti(
+            name='alerts',
+            datatype=bool,
+            cardinality_from='targets',
+            description='Alert outputs')
+        )
+
+        return (inputs, outputs)
     
 class JHMSimpleRegressor(BaseRegressor):
     '''
